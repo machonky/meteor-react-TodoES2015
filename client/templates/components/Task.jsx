ef6dc287b89b827b495b252739f5bd37ca9b3107
machonky <dev@macrobert.com>
2015-12-30 16:25:02 +0800

    AutoBinding & Meteor Reactivity

--- client/templates/components/Task.jsx
+++ client/templates/components/Task.jsx
@@ -1,13 +1,43 @@
 class Task extends React.Component {
+
   static propTypes() {
     return {
       task: React.PropTypes.object.isRequired
     };
   }
 
+  toggleChecked() {
+    // Set the checked property to the opposite of its current value
+    Tasks.update(this.props.task._id, {
+      $set: {checked: ! this.props.task.checked}
+    });
+  }
+
+  deleteThisTask() {
+    Tasks.remove(this.props.task._id);
+  }
+
   render() {
-    return (<li>{this.props.task.text}</li>);
+    const taskClassName = this.props.task.checked ? "checked":"";
+
+    return (
+      <li className={taskClassName}>
+        <button
+          className="delete"
+          onClick={this.deleteThisTask}>
+          &times;
+        </button>
+
+        <input
+          type="checkbox"
+          readOnly={true}
+          checked={this.props.task.checked}
+          onClick={this.toggleChecked} />
+
+      <span className="text">{this.props.task.text}</span>
+      </li>);
   }
 }
 
-this.Task = Task;
+// Why do we have to pollute the global namespace, instead of exporting
+this.Task = autoBind(Task);
